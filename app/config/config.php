1ebe72fcef390d578c496609a8757ac96541edce
rafaeloliveirachapouto <rafaeloliveira.c@hotmail.com>
2020-02-05 14:34:58 -0300

    Finalizado todo o código de consumo da API HG Brasil para verificar a variação do dólar.

---
+++ app/config/config.php
@@ -0,0 +1,3 @@
+<?php
+define('HG_API_KEY', 'd3d97648');
+?>
--- index.php
+++ index.php
@@ -1,1 +1,37 @@
-<?php phpinfo(); ?>
+<?php
+require_once('app/config/config.php');
+require_once('app/modules/hg-api.php');
+
+$hg = new HG_API(HG_API_KEY);
+$dolar = $hg->dolar_quotation();
+if ($hg->is_error() == false) {
+    $variation = ($dolar['variation'] < 0) ? 'danger' : 'success';
+}
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>HG Brasil</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>HG Brasil - Cotação do Dólar</h1>
+                <?php if ($hg->is_error() == false): ?>
+                    <p>USD <span class="badge badge-pill badge-<?php echo $variation; ?>"><?php echo $dolar['buy']; ?></span></p>
+                <?php else: ?>
+                    <p>USD <span class="badge badge-pill badge-<?php echo $variation; ?>">Serviço Indisponível</span></p>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
